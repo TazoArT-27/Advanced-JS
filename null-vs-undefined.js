326c26052e609669954ad910b73d7144ef252100
1810050 <67385245+1810050@users.noreply.github.com>
2020-08-09 22:35:04 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+// let afi;
+// console.log(afi);
+
+// function add(num1, num2){
+//     console.log(num1+num2);
+//     return
+// }
+// const result = add(13,82);
+// console.log(result);
+
+function add(num1, num2=10){
+
+    console.log(num1, num2);
+}
+const result = add(13);
+console.log(result);
+
+const student = {
+    name : "shafayat",
+    phone : 89765678457
+
+}
+console.log(student.roll);
+
+let arr = [2,3,4];
+console.log(arr[11]);
