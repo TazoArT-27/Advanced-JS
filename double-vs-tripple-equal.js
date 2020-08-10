e550b8aa25f74ae6406bee3bf33b0c2c07f66917
1810050 <67385245+1810050@users.noreply.github.com>
2020-08-10 09:24:52 +0600

    double vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,36 @@
+const first = 2;
+const second = 3;
+if(first == second){
+    console.log("condition is true");
+
+}else{
+    console.log("condition is false");
+}
+
+const first1 = 4;
+const second1 = '4';
+if(first2 == second2){
+    console.log("condition is true");
+
+}else{
+    console.log("condition is false");
+}
+
+const first2 = 4;
+const second2 = '4';
+if(first2 === second2){
+    console.log("condition is true");
+
+}else{
+    console.log("condition is false");
+}
+
+const first3 = 1;
+const second3 = true;
+// if(first3 == second3){
+if(first3 === second3){
+    console.log("condition is true");
+
+}else{
+    console.log("condition is false");
+}
